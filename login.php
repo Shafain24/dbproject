fef91d264f308af7340d011a65517931801edc1c
Shafain24 <mshafain27@gmail.com>
2024-01-14 23:01:47 +0500

    finally done

--- login.php
+++ login.php
@@ -10,7 +10,7 @@ if (mysqli_connect_error()) {
     die("Connection ERROR: " . mysqli_connect_error());
 }
 
-// Check if the form is submitted using POST
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $roll_no = $_POST["roll_no"];
     $password = $_POST["password"];
@@ -19,13 +19,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $result = mysqli_query($conn, $sql);
 
     if ($row = mysqli_fetch_assoc($result)) {
-        // Compare the entered password with the stored hashed password
+
         if (password_verify($password, $row['Password'])) {
-            // Successful login, start session, set session variable, and redirect
+
             session_start();
             $_SESSION['roll_no'] = $roll_no;
             header("Location: dashboard.php");
-            exit(); // Important to exit after redirection
+            exit();
         } else {
             echo "Incorrect password. <a href='login.html'>Try again</a>.";
         }
--- signin.php
+++ signin.php
@@ -10,15 +10,15 @@ if (mysqli_connect_error()) {
     die("Connection ERROR: " . mysqli_connect_error());
 }
 
-// Check if the form is submitted
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Check if the necessary form fields are set
+
     if (isset($_POST["roll_no"]) && isset($_POST["password"]) && isset($_POST["cpassword"])) {
         $username = mysqli_real_escape_string($conn, $_POST["roll_no"]);
         $password = $_POST["password"];
         $confirm_password = $_POST["cpassword"];
 
-        // Check if the entered passwords match
+
         if ($password !== $confirm_password) {
             echo "Passwords do not match. <a href='signup.html'>Try again</a>.";
             exit();
@@ -26,19 +26,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         $password = password_hash($password, PASSWORD_BCRYPT);
 
-        // Check if the username already exists
+
         $check_user_sql = "SELECT * FROM user_accounts WHERE Roll_No='$username'";
         $check_user_result = mysqli_query($conn, $check_user_sql);
 
         if (mysqli_num_rows($check_user_result) > 0) {
-            // Username already exists, display an error
+
             echo "Username '$username' already exists. <a href='signup.html'>Try a different username</a>.";
         } else {
-            // Proceed with registration
+
             $insert_user_sql = "INSERT INTO user_accounts (Roll_No, Password) VALUES ('$username', '$password')";
             mysqli_query($conn, $insert_user_sql);
 
-            // Registration successful, redirect to login page
             header("Location:signup.html");
             exit();
         }
